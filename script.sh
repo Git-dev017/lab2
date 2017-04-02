77a24a9179a313d0635012abe60195d5d3c5bee1
Git-dev017 <vladislav.zilkov@gmail.com>
2017-04-02 22:27:34 +0300

    script 2nd ver

--- script.sh
+++ script.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 echo "Start download page"
-wget -F http://ru.euronews.com/weather/europe/belarus/minsk -O weather
-string1=$(grep "stat enw-temp__color-19 unit_C ltr" weather > cache.txt)
-echo "CheckPoint!"
-echo $string1
-
-#rm weather
+wget -F http://ru.euronews.com/weather/europe/belarus/minsk -O weather.txt
+grep "enw-block__cityWeather__temp-max unit_C ltr" weather.txt > cache.txt
+delimiter=$(awk -F'">' '{print $2}' cache.txt)
+#echo "checkpoint"
+echo $delimiter > cache.txt
+#echo $delimiter
+delimiter=$(awk -F'</' '{print $1}' cache.txt)
+echo $delimiter
+rm *.txt
 
