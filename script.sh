19cbde74dd86dac52372c92091f2b5b8c9751a1b
Git-dev017 <vladislav.zilkov@gmail.com>
2017-04-02 16:56:28 +0300

    Script

--- script.sh
+++ script.sh
@@ -1,1 +1,9 @@
-#
+#!/bin/bash
+echo "Start download page"
+wget -F http://ru.euronews.com/weather/europe/belarus/minsk -O weather
+string1=$(grep "stat enw-temp__color-19 unit_C ltr" weather > cache.txt)
+echo "CheckPoint!"
+echo $string1
+
+#rm weather
+
