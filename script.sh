afa00ba3d1a03b97b4f634a9e56cf278b28ce8d3
Git-dev017 <vladislav.zilkov@gmail.com>
2017-04-06 23:13:06 +0300

    working version

--- script.sh
+++ script.sh
@@ -24,6 +24,6 @@ wget -F http://ru.euronews.com/weather/europe/belarus/minsk -O weather.txt
 string1=$(grep "enw-block__cityWeather__temp-max unit_C ltr" weather.txt | awk -F'">' '{print $2}' | awk -F'<' '{print $1}')
 echo $string1
 rm *.txt
-echo !!!!!!!!!!!!!!!!!!!!!!!!!!
-echo $1
+#echo !!!!!!!!!!!!!!!!!!!!!!!!!!
+#echo $1
 ./script.sh $1
