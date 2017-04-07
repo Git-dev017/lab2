ed286432cff087c6f0cc2cdd65a9d3d9123c5625
Git-dev017 <vladislav.zilkov@gmail.com>
2017-04-07 19:22:52 +0300

    edited (added sleep)

--- script.sh
+++ script.sh
@@ -1,24 +1,9 @@
 #!/bin/bash
-#echo "starting SCRIPT!"
-minute=60
-date_prev=$(date +"%M")
-dif_time=0
-while [[ $dif_time < $1 ]]
+echo "starting SCRIPT, enter the period (seconds):"
+read period
+while [[ 0 ]]
 do
 #echo "start WHILE"
-date_now=$(date +"%M")
-
-if [[ date_prev < date_now ]]
-then
-let "dif_time=date_prev+date_now"
-let "dif_time=dif_time-60"
-#echo "date_prev>date_now " $dif_time
-else
-let "dif_time=date_now-date_prev"
-echo "date_prev<date_now " $dif_time
-fi
-
-done
 
 wget -F http://ru.euronews.com/weather/europe/belarus/minsk -O weather.txt
 string1=$(grep "enw-block__cityWeather__temp-max unit_C ltr" weather.txt | awk -F'">' '{print $2}' | awk -F'<' '{print $1}')
@@ -26,4 +11,5 @@ echo $string1
 rm *.txt
 #echo !!!!!!!!!!!!!!!!!!!!!!!!!!
 #echo $1
-./script.sh $1
+sleep $period
+done
